e931c3deb0412c21ab8ff487c7d6d4a33771e49d
Mount-Blanc <mountblanc87@gmail.com>
2023-02-02 11:26:33 -0600

    added flickering menu item

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Music from './Music';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsVisible(prevIsVisible => !prevIsVisible);
+    }, 700);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
     <div className="App">
     Hello
-    </div>
-    <div className='press-to-play'>
+    </div>{isVisible &&<div className='press-to-play'>
     Press any key to play
-    </div>
+    </div>}
+    
     <Music/>
     </div>
   );
