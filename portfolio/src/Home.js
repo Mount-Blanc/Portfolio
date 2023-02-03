3c8bdb4bc16fe40f4b28f9deecaa61a7b7623f18
Mount-Blanc <mountblanc87@gmail.com>
2023-02-03 13:32:24 -0600

    got the css animation working

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -1,20 +1,18 @@
 import './Home.css'
 function Home () {
     return(
-        <div className='space'>
+        <div className='container'>
             <h3>*score advance table*</h3>
-            <div class="space">
-    <span>Classic</span>
-    <br />
-    <div class="space-invader space-invader-1 classic"></div>
-    <div class="space-invader space-invader-2 classic"></div>
-    <div class="space-invader space-invader-3 classic"></div>
-  </div>
+   
             <ul>
                 <li>
-                  <div className='space-invader space-invader-1 classic'></div> = <a href=''>about </a>
-                 = <a href=''>projects </a>
-                  = <a href=''>contact </a>
+                  <span className='space-invader space-invader-3 classic'></span>=<a href=''>about </a>
+                </li>
+                <li>
+                <span className='space-invader space-invader-1 classic'></span>=<a href=''>projects </a>
+                </li>
+                <li>
+                <span className='space-invader space-invader-2 classic'></span>=<a href=''>contact </a>
                 </li>
             </ul>
         </div>
