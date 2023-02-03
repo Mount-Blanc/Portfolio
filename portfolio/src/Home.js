88af069f28284c5b9e1c218b954c202c04959eb2
Mount-Blanc <mountblanc87@gmail.com>
2023-02-03 12:56:07 -0600

    fixed alien css

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -2,11 +2,18 @@ import spaceinvader from './space-invader.svg'
 import './Home.css'
 function Home () {
     return(
-        <div>
+        <div className='space'>
             <h3>*score advance table*</h3>
+            <div class="space">
+    <span>Classic</span>
+    <br />
+    <div class="space-invader space-invader-1 classic"></div>
+    <div class="space-invader space-invader-2 classic"></div>
+    <div class="space-invader space-invader-3 classic"></div>
+  </div>
             <ul>
                 <li>
-                  <img src={spaceinvader}/> = <a href=''>about </a>
+                  <div className='space-invader space-invader-1 classic'></div> = <a href=''>about </a>
                   <img src={spaceinvader}/>= <a href=''>projects </a>
                   <img src={spaceinvader}/> = <a href=''>contact </a>
                 </li>
