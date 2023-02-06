bae612101344110c883d75763b367cf108c0b1dc
Mount-Blanc <mountblanc87@gmail.com>
2023-02-06 13:02:49 -0600

    fixed welcome component camera bug

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -12,7 +12,7 @@ function Home () {
     return(
         <div   >
           <h1>About Me</h1>
-            <p>I'm passionate about building things with emerging web technologies. Although I'm aiming for a front-end position I also study the backend so 
+            <p>I'm a self-taught programmer and love to build things with emerging web technologies. Although I'm aiming for a front-end position I also study the backend so 
                 I can have a complete understanding of the whole picture.
                 When I am not studying programming I like to study videogame UI's,read books, and watch movies. I'm proud of my small but growing collection of movie DVD's</p>
             <h3>*score advance table*</h3>
