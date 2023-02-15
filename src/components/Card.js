f8c98eb434eb24150637b9f54c3bd3510b8920b0
Mount-Blanc <mountblanc87@gmail.com>
2023-02-15 12:07:59 -0600

    added links to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import './Card.css'
 
 const Card =(props) => {
     return(
-        <div className='card title'>{props.children}</div>
+        <div className='card title '>{props.children}</div>
     )
 }
 export default Card
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import Card from './Card'
+import './Projects.css'
 import CountryInfo from '../assets/imgs/CountryInfo.png'
 import Ecommerce from '../assets/imgs/E-commerce.png'
 import letusfaire from '../assets/imgs/letusfaire.png'
@@ -9,37 +10,44 @@ function Projects () {
             {
               name: 'CountryInfo',
               image: CountryInfo,
-              link: 'https://github.com/Mount-Blanc/CountryInfo'
+              Weblink: 'https://countryinfo.herokuapp.com/',
+              githublink: 'https://github.com/Mount-Blanc/CountryInfo'
             },
             {
               name: 'Let-Us-Faire',
-              image: Ecommerce,
-              link: 'https://github.com/Mount-Blanc/Let-Us-Faire'
+              image: letusfaire,
+              Weblink: 'https://let-us-faire.herokuapp.com/',
+              githublink: 'https://github.com/Mount-Blanc/Let-Us-Faire' 
             },
             {
                 name: 'E-Commerce',
-                image: letusfaire,
-                link: 'https://github.com/Mount-Blanc/E-commerce'
-              },
+                image: Ecommerce,
+                Weblink: 'https://fictionalecommerce.herokuapp.com/',
+                githublink: 'https://github.com/Mount-Blanc/E-commerce'
+               },
               {
-                name: 'Project 4',
-                image: 'project2.jpg',
-                link: 'https://github.com/user/project2'
+                name: 'Coming Soon',
               },
           ];
        
-          return (
-            <div className="my-projects">
+          return (            
+            <>
               <h1>My Projects</h1>
+            <div className="projectContainer">
               {projects.map((project) => (
                 <Card className="project" key={project.name}>
                   <h3>{project.name}</h3>
                   <a href={project.link}>
-                    <img src={project.image} alt={project.name} />
+                    <img className='img' src={project.image} alt={project.name} />
                   </a>
+                  <div>
+                    <button><a href={project.Weblink} >Visit Site</a></button>
+                    <button><a href={project.githublink} >View Code</a></button>
+                  </div>
                 </Card>
               ))}
             </div>
+            </>
           );
         };
         
