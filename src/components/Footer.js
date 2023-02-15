cc223e5d05fcc869e4b3f6cbb4aef8ed89c34190
Mount-Blanc <mountblanc87@gmail.com>
2023-02-15 12:11:20 -0600

    added padding to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer () {
     return(
-        <div>
+        <div style={{padding:'1rem'}}>
            <a target="_blank" rel="noopener noreferrer" href='https://github.com/Mount-Blanc'>Github</a> 
         </div>
     )
