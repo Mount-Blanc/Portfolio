206db8fbdc61ef3a7cf58e2af5d726678d9a35db
Mount-Blanc <mountblanc100@gmail.com>
2023-02-19 11:47:28 -0600

    added typescript icon

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,37 +1,66 @@
-import './Skills.css'
+import "./Skills.css";
 
+function Skills() {
+  return (
+    <div className="spacing">
+      <h1>These are the Technologies i use</h1>
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+      />
 
-function Skills ()  {
-    return(
-        <div className='spacing'>
-            <h1>These are the Technologies i use</h1>
-             <img className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg" />
+      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
 
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg" />
-          
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg" />
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
+      />
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
+      />
 
-          
-          
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg" />
-          
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain-wordmark.svg"
+      />
 
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"
+      />
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"
+      />
 
-             <img  className='skillimg'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
-                    
-                    
-          
-             <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg" />
-              <img  className='skillimg'  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-              <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg" />
-              <img  className='skillimg' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg" />
-      
-      
-      
-        </div>
-    )
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
+      />
+
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg"
+      />
+
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain-wordmark.svg"
+      />
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+      />
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain-wordmark.svg"
+      />
+      <img
+        className="skillimg"
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain-wordmark.svg"
+      />
+    </div>
+  );
 }
-export default Skills
+export default Skills;
