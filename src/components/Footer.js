50e2a3835f87f3d33baa326b4cf51ea7878f9d23
Mount-Blanc <mountblanc100@gmail.com>
2023-04-02 15:44:53 -0500

    added css to github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,9 @@
+import "./Footer.css"
+
 function Footer () {
     return(
         <div style={{padding:'1rem'}}>
-           <a target="_blank" rel="noopener noreferrer" href='https://github.com/Mount-Blanc'>Github</a> 
+           <a className="githublink" target="_blank" rel="noopener noreferrer" href='https://github.com/Mount-Blanc'>Github</a> 
         </div>
     )
 }
